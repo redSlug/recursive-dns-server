90e89c73a6a6874c1f17a0381627f71c6dd5c594
Brad Dettmer <11279144+redSlug@users.noreply.github.com>
2020-06-27 16:05:29 -0400

    added function to get domain name from UDP request

--- service/server.py
+++ service/server.py
@@ -7,6 +7,22 @@ import socket
 from service.constants import BUFFER_SIZE, HOST, PORT
 
 
+def get_domain_name(message):
+    url_parts = list()
+    start_byte = 12
+    while True:
+        length = message[start_byte]
+        if not length:
+            break
+
+        url_part = message[start_byte + 1:start_byte + 1 + length]
+        url_parts.append(url_part)
+        start_byte = start_byte + length + 1
+
+    print('.'.join([p.decode() for p in url_parts]))
+    return url_parts
+
+
 def run():
     """
     Creates a datagram socker, binds to HOST, listens for datagrams and
@@ -20,7 +36,7 @@ def run():
         while True:
             message, address = s.recvfrom(BUFFER_SIZE)
             print(f"Received {message} from {address}")
-
+            get_domain_name(message)
             # Sending a reply to client
             bytes_to_sent = str.encode("yo, I got your message")
             s.sendto(bytes_to_sent, address)
